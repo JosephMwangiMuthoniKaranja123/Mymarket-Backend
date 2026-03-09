7b50439cb6e0555da080ca6354c5dce5cf9abca2
Joseph Karanja <joserogerz123@gmail.com>
2026-03-09 10:44:28 +0300

    changes to dbimport

--- src/dbimport.js
+++ src/dbimport.js
@@ -6,11 +6,11 @@ async function importDatabase() {
   try {
     // Create connection using Railway variables
     const connection = await mysql.createConnection({
-      host: process.env.MYSQLHOST,
-      port: Number(process.env.MYSQLPORT),  // Convert string to number
-      user: process.env.MYSQLUSER,
-      password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),  // Convert string to number
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     });
 
     console.log("✅ Connected to the database");

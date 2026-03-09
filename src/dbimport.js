3ae167f674f79bc62fc1fca9e3501879ac2443f5
Joseph Karanja <joserogerz123@gmail.com>
2026-03-09 11:20:50 +0300

    fourth change for dbimport file

--- src/dbimport.js
+++ src/dbimport.js
@@ -2,6 +2,10 @@
 import fs from "fs";
 import mysql from "mysql2/promise";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function importDatabase() {
   try {
